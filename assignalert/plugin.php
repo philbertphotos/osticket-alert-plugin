e013b6cd44583b78ebf16389cd14726ab55fbaf6
Joseph Philbert <joe@philbertphotos.com>
2022-09-02 17:54:03 -0400

    Update plugin.php

--- assignalert/plugin.php
+++ assignalert/plugin.php
@@ -4,14 +4,14 @@
  * Description of plugin
  * @author Joseph Philbert <joe@philbertphotos.com>
  * @license http://opensource.org/licenses/MIT
- * @version 0.1
+ * @version 1.4
  */
  
 set_include_path(get_include_path().PATH_SEPARATOR.dirname(__file__).'/include');
 
 return array(
     'id' => 'pps:assign_alert',
-    'version' => '1.3 beta',
+    'version' => '1.4',
     'name' => 'PPS Assign Alert',
     'author' => 'Joseph Philbert',
     'description' => 'Send an alert when a ticket is assigned to managers in a department.',
